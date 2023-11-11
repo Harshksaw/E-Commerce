f79cb71f9144e18a0138af6a96e003da32fef893
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-11-11 13:28:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+import ProductList from './product-list/ProductList'
+
+function App() {
+
+
+  return (
+    <>
+
+    <ProductList/>
+      
+    </>
+  )
+}
+
+export default App
