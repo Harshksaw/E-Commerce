2b9d48cd7ee373c43cf26a9bccdcc325f4c9df89
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-12-19 15:39:37 +0000

    temp Deploy

--- client/src/Customer/components/Carousel/MainCarouselData.js
+++ client/src/Customer/components/Carousel/MainCarouselData.js
@@ -3,11 +3,11 @@ export const mainCarouselData = [
 
     { image: "https://i.pinimg.com/originals/55/82/60/5582603576bf2823ea1540b7e0b54980.png", path: "imgpath1" },
     { image: "https://i.pinimg.com/originals/fc/43/f4/fc43f453bbf5316cb9bb65ea5ff72056.jpg", path: "imgpath1" },
-    { image: "https://i.pinimg.com/originals/21/e7/eb/21e7eb1fcbbb04dae8afaac8394f6335.png", path: "imgpath1" },
-    { image: "https://i.pinimg.com/originals/88/d3/c8/88d3c815f5ffbfd4ab1b4d8f7d977385.jpg", path: "imgpath1" },
+    { image: "https://i.ytimg.com/vi/EiAD7CNp4Vo/maxresdefault.jpg", path: "imgpath1" },
 
 
 
-    // Add more objects as needed
+
+
   ];
 
