843d2b7ec52499ead5e7e34754eb485fc368e755
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-01 10:16:49 +0000

    HugHeart Ecomm Init

--- frontend/store/store.js
+++
@@ -1,8 +0,0 @@
-import cartSlice from './slices/cartSlice'
-import {configureStore} from '@reduxjs/toolkit'
-export const store = configureStore({
-  reducer: {
-    cart: cartSlice
-  },
-  devTools: true
-})
--- frontend/utils/urls.js
+++
@@ -1,3 +0,0 @@
-export const STRAPI_API_TOKEN= process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-
-
